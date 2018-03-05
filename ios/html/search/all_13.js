0c7043a0f0f986854dd359e93f8142c37d2e90be
Eduard Frank <eddy@scanbot.io>
2018-03-05 17:31:18 +0100

    Update iOS SDK docs for release v1.5.8

--- ios/html/search/all_13.js
+++ ios/html/search/all_13.js
@@ -4,5 +4,13 @@ var searchData=
   ['warpimage_3apolygon_3aoutputimageurl_3acompletion_3a',['warpImage:polygon:outputImageURL:completion:',['../category_s_b_s_d_k_image_processor_07_n_s_u_r_l_08.html#a12db6723911ba50e303c1014b835633d',1,'SBSDKImageProcessor(NSURL)::warpImage:polygon:outputImageURL:completion:()'],['../interface_s_b_s_d_k_image_processor.html#a12db6723911ba50e303c1014b835633d',1,'SBSDKImageProcessor::warpImage:polygon:outputImageURL:completion:()']]],
   ['weburl',['webURL',['../interface_s_b_s_d_k_web_u_r_l_q_r_code.html#ade7bc9d164d9ce109de5310c5fb7e6a9',1,'SBSDKWebURLQRCode']]],
   ['words',['words',['../interface_s_b_s_d_k_o_c_r_result.html#aa8775bcbaf550fcf07db7092c38228a0',1,'SBSDKOCRResult']]],
+  ['writebinarizedmultipagetiff_3afileurl_3a',['writeBinarizedMultiPageTIFF:fileURL:',['../interface_s_b_s_d_k_t_i_f_f_image_writer.html#a62981c1adf63bd7b9ed5c05711364ac4',1,'SBSDKTIFFImageWriter']]],
+  ['writebinarizedmultipagetifffromimageurls_3afileurl_3a',['writeBinarizedMultiPageTIFFFromImageURLs:fileURL:',['../interface_s_b_s_d_k_t_i_f_f_image_writer.html#a9da49bbcacaedadaace5113fc4d42855',1,'SBSDKTIFFImageWriter']]],
+  ['writebinarizedtiff_3afileurl_3a',['writeBinarizedTIFF:fileURL:',['../interface_s_b_s_d_k_t_i_f_f_image_writer.html#a513b44716ef52fb5f1779d5c74d1ab61',1,'SBSDKTIFFImageWriter']]],
+  ['writebinarizedtifffromurl_3afileurl_3a',['writeBinarizedTIFFFromURL:fileURL:',['../interface_s_b_s_d_k_t_i_f_f_image_writer.html#ab0d7fcf6a04d68c08d460157a84df449',1,'SBSDKTIFFImageWriter']]],
+  ['writemultipagetiff_3afileurl_3a',['writeMultiPageTIFF:fileURL:',['../interface_s_b_s_d_k_t_i_f_f_image_writer.html#a8f680278c857a43169f18647fc286ea7',1,'SBSDKTIFFImageWriter']]],
+  ['writemultipagetifffromimageurls_3afileurl_3a',['writeMultiPageTIFFFromImageURLs:fileURL:',['../interface_s_b_s_d_k_t_i_f_f_image_writer.html#a006bc493ff699ff3b344921c17e93f23',1,'SBSDKTIFFImageWriter']]],
+  ['writetiff_3afileurl_3a',['writeTIFF:fileURL:',['../interface_s_b_s_d_k_t_i_f_f_image_writer.html#afc6c5799d55ab58f527c6dfcf22316d4',1,'SBSDKTIFFImageWriter']]],
+  ['writetifffromurl_3afileurl_3a',['writeTIFFFromURL:fileURL:',['../interface_s_b_s_d_k_t_i_f_f_image_writer.html#a620e3c0cc2e947b7a9b5ab816c0ed677',1,'SBSDKTIFFImageWriter']]],
   ['writingdirection',['writingDirection',['../interface_s_b_s_d_k_page_analyzer_result.html#a6fa6c6cbcc91980ae1d15ccd33afc46b',1,'SBSDKPageAnalyzerResult']]]
 ];
