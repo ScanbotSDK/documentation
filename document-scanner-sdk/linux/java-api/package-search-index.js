e48289e74dc4310e40c5c2968a2945f4c546ca86
Eduard Frank <eddy@scanbot.io>
2024-10-14 18:01:50 +0200

    add java api docs of the java wrapper for the linux sdk beta

---
+++ document-scanner-sdk/linux/java-api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.scanbot.sdk"},{"l":"io.scanbot.sdk.documentdetector"},{"l":"io.scanbot.sdk.documentqualityanalyzer"},{"l":"io.scanbot.sdk.geometry"},{"l":"io.scanbot.sdk.image"},{"l":"io.scanbot.sdk.utils"}]
